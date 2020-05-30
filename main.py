3bfbb77f390505a793cadb53e1feac0c738a9c83
Ashu7397 <ashutoshtaklikar@gmail.com>
2020-05-30 17:19:44 +0300

    tester: Update the marking scheme and add support for variable csv filename

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from assignment_3 import tests
 
 
 SRC_DIR = "src_files"
-
+CSV_FILE_NAME = "results_final.csv"
 
 def main():
     tester = Tester(tests)
-    for student in os.listdir(SRC_DIR):
+    for student in sorted(os.listdir(SRC_DIR)):
         print("Testing student {}: ".format(student), end='')
         tester.test(student, os.path.join(SRC_DIR, student))
 
     print("Creating report")
-    tester.report_results()
+    tester.report_results(CSV_FILE_NAME)
 
 
 if __name__ == "__main__":
--- tester/tester.py
+++ tester/tester.py
@@ -16,6 +16,7 @@ class Tester:
         for q_num, tests in enumerate(self.tests):
             try:
                 q_name = "/q{}".format(q_num+1)
+                print("\nQ", q_num + 1, end="")
                 with Program(path+q_name) as prog:
                     try:
                         prog.compile()
@@ -28,9 +29,9 @@ class Tester:
                             "\033[92m.\033[0m" if res else "\033[91mX\033[0m", end="")
                         self.results[student][q_name].append("PASS" if res else "FAIL")
             except RuntimeError as e:
-                print("\033[91m{}\033[0m".format(str(e)))
+                print("\033[91m{}\033[0m".format(str(e)), end="")
                 self.results[student][q_name].append(str(e))
-                break
+                #break
         print()
 
     @staticmethod
@@ -47,14 +48,31 @@ class Tester:
         prog.log_file.write("result: {}\n".format("PASS" if res else "FAIL"))
         return res
 
-    def report_results(self):
-        with open("result.csv", "w") as fd:
+    def report_results(self, file_name):
+        with open(file_name, "w") as fd:
             handle = csv.writer(fd, dialect='excel')
             test_names = ["q{}_{}".format(index+1, test.name)
              for index, tests in enumerate(self.tests)
                  for test in tests]
-            handle.writerow(["student", "grade"] + test_names)
-            for student, question in self.results.items():
-                results = [res for _, test in question.items() for res in test]
-                grade = 100*sum(1.0 for res in results if res == "PASS")/len(test_names)
-                handle.writerow([student, grade] + results)
+            handle.writerow(["student", "student_id", "grade"] + test_names)
+            for student, question in sorted(self.results.items()):
+                # results = [res for _, test in question.items() for res in test]
+                total_list = []
+                for item in sorted(question.items()):
+                    val = item[1]
+                    if val[0] == "Failed to compile":
+                        val = item[1] * int(len(test_names)/len(item[0]))
+                    total_list = total_list + val
+                # grade = 100*sum(1.0 for res in results if res == "PASS")/len(test_names)
+                total_grade = 0
+                for grade in total_list:
+                    if grade == "PASS":
+                        grade_points = 7
+                    elif grade == "FAIL":
+                        grade_points = 3
+                    else:
+                        grade_points = 0
+                    total_grade += grade_points
+                total_grade = total_grade if total_grade < 100 else 100
+                student_name, student_id = student.split('_')
+                handle.writerow([student_name, student_id, total_grade] + total_list)
